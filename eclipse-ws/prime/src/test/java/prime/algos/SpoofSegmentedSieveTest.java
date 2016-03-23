2e376debeb6f08eb7e6e3c1d53a6f758fc7f9714
George Mullen <geo.mullen@gmail.com>
2016-03-23 18:13:27 +0000

    added countdown latch trigger in mockito stubbed consumer to  allow multithreaded test to work consistently

--- eclipse-ws/prime/src/test/java/prime/algos/SpoofSegmentedSieveTest.java
+++ eclipse-ws/prime/src/test/java/prime/algos/SpoofSegmentedSieveTest.java
@@ -8,12 +8,17 @@ package prime.algos;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 import java.util.function.Consumer;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.mockito.Mock;
 import static org.mockito.Mockito.*;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
 
 /**
  *
@@ -32,7 +37,6 @@ public class SpoofSegmentedSieveTest {
     public void tearDown() {
     }
 
-    
     @Test
     public void testGenIndices() {
         System.out.println("genPrimes");
@@ -42,16 +46,23 @@ public class SpoofSegmentedSieveTest {
 
     }
 
-   
-
     @Test
     public void testDispatchCalculation() throws Exception {
         SpoofSegmentedSieve testClazz = new SpoofSegmentedSieve();
         Consumer< Set<Integer>> mockAccumulator = mock(Consumer.class);
-        int[] limits = new int[] {7,13};
+        final CountDownLatch countdownLatch = new CountDownLatch(1);
+        doAnswer(new Answer() {
+	    public Object answer(InvocationOnMock invocation) {
+	       countdownLatch.countDown();
+	        return null;
+	    }}).when(mockAccumulator).accept(any(Set.class));
+               
+
+        int[] limits = new int[]{7, 13};
         testClazz.dispatchCalculation(limits, mockAccumulator);
-        Set<Integer> expected = new HashSet<>(Arrays.asList(7,11,13));
-        Thread.sleep(1000);// qucik and dirty hack to allow other thread to update accumulator
+        Set<Integer> expected = new HashSet<>(Arrays.asList(7, 11, 13));
+
+        countdownLatch.await(10, TimeUnit.SECONDS);
         verify(mockAccumulator).accept(eq(expected));
 
     }
