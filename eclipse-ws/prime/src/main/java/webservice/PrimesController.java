82ac7fd3fa840dc3368a7ae71e706f9b0b7cada9
George Mullen <geo.mullen@gmail.com>
2016-03-23 17:29:18 +0000

    added spoof segemented sieve calculator added limit check on maximum prime allowed in web service call added exception wrapper for all prime number calcualtion exceptions

--- eclipse-ws/prime/src/main/java/webservice/PrimesController.java
+++ eclipse-ws/prime/src/main/java/webservice/PrimesController.java
@@ -1,7 +1,7 @@
 package webservice;
 
-
 import calculators.PrimeCalculatorFactory;
+import calculators.PrimeGeneratorException;
 import java.util.Arrays;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,46 +14,38 @@ import org.springframework.web.bind.annotation.*;
 @Controller
 @EnableAutoConfiguration
 public class PrimesController {
-    
-    
-        private PrimeCalculatorFactory factory = new PrimeCalculatorFactory();
-        private Converter converter = new Converter();
-    
-    
-    
-	private final static Logger log = LoggerFactory.getLogger(PrimesController.class.getName());;
+    private PrimeCalculatorFactory factory = new PrimeCalculatorFactory();
+    private Converter converter = new Converter();
 
+    private final static Logger log = LoggerFactory.getLogger(PrimesController.class.getName());
 
-    @RequestMapping(value="/prime/{algo}/{max_limit}",method = RequestMethod.GET)
+    @RequestMapping(value = "/prime/{algo}/{max_limit}", method = RequestMethod.GET)
     @ResponseBody
-    String getPrimes(@PathVariable String algo, @PathVariable int max_limit) {
-    	log.debug("rest method <prime>");
-            int[] res = factory.getCalculator(algo).primes(max_limit);
+    String getPrimes(@PathVariable String algo, @PathVariable int max_limit) throws PrimeGeneratorException {
+        log.debug("rest method <prime>");
+        if (max_limit > 5000000) {
+            throw new PrimeGeneratorException("Exceeded maximum limit of primes");
+        }
+
+        int[] res = factory.getCalculator(algo).primes(max_limit);
         return converter.serialize(res);
     }
 
-    
     @RequestMapping("/quit")
     @ResponseBody
     void quit() {
-    	log.debug("rest method <quit>");
+        log.debug("rest method <quit>");
         System.exit(-999);
     }
 
-   
-    @ResponseStatus(value=HttpStatus.INTERNAL_SERVER_ERROR)
-	@ExceptionHandler(Exception.class)
-	public String handleException(Exception exception)
-	{
-		return exception.getMessage();
-	}
-    
-    
+    @ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(Exception.class)
+    public String handleException(PrimeGeneratorException exception) {
+        return exception.getMessage();
+    }
+
     public static void main(String[] args) throws Exception {
         SpringApplication.run(PrimesController.class, args);
     }
-    
-    
-    
-    
+
 }
